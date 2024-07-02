44da838ee2cdc973a5df77fa593bee60bb1f53f5
Jameer Babu <jameer.jb@gmail.com>
2024-07-02 18:42:31 -0500

    initial setup

---
+++ src/main/java/com/example/housemgmtsys/HousemgmtsysApplication.java
@@ -0,0 +1,13 @@
+package com.example.housemgmtsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousemgmtsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousemgmtsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/housemgmtsys/HousemgmtsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housemgmtsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousemgmtsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
